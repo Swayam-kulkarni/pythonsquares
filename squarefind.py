d70121322403284e3e6f250161f62b3b182ff2ed
Swayam-kulkarni <56629877+Swayam-kulkarni@users.noreply.github.com>
2020-06-16 11:16:57 +0530

    Squares with python
    Easy codes to find squares and and square roots.
    (Respectively becouse there are 2 files).

---
+++ squarefind.py
@@ -0,0 +1,3 @@
+num_unt = float(input())
+square = num_unt * num_unt
+print(square)
---
+++ squarerootfind.py
@@ -0,0 +1,9 @@
+import math
+
+# Some values
+valueA = float(input("square number who's Square root to find = "))
+
+# Get the square root of those values
+sqrtA = math.sqrt(valueA)
+# Output the results
+print("√", valueA, " = ", sqrtA, sep="")
